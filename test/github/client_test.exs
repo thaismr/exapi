faabeacf5d80fb06f2027736e0c52dbadae22c87
thaismr <thaismr@users.noreply.github.com>
2022-09-08 18:11:09 -0300

    feat: tests for API error handling

--- test/github/client_test.exs
+++ test/github/client_test.exs
@@ -5,7 +5,8 @@ defmodule Exapi.Github.ClientTest do
   """
   use ExUnit.Case, async: true
 
-  alias Exapi.Github.Client
+  alias Exapi.Error
+  alias Exapi.Github.{Client, Response}
   alias Github.Support.GithubFixture
 
   describe "get_user_repos/2" do
@@ -31,21 +32,21 @@ defmodule Exapi.Github.ClientTest do
       expected_response = {
         :ok,
         [
-          %Exapi.Github.Response{
+          %Response{
             description: nil,
             html_url: "https://github.com/12345/12345.github.io",
             id: 173_204_968,
             name: "12345.github.io",
             stargazers_count: 0
           },
-          %Exapi.Github.Response{
+          %Response{
             description: "memo",
             html_url: "https://github.com/12345/test",
             id: 91_750_966,
             name: "test",
             stargazers_count: 0
           },
-          %Exapi.Github.Response{
+          %Response{
             description: "Play Framework Documentation Japanese Translation",
             html_url: "https://github.com/12345/translation-project",
             id: 92_003_687,
@@ -54,6 +55,43 @@ defmodule Exapi.Github.ClientTest do
           }
         ]
       }
+
+      assert response == expected_response
+    end
+
+    test "when the username was not found, returns an error", %{bypass: bypass} do
+      username = "_"
+
+      url = endpoint_url(bypass.port)
+
+      body = ~s({
+          "message": "Not Found",
+          "documentation_url": "https://docs.github.com/rest/reference/repos#list-repositories-for-a-user"
+        })
+
+      Bypass.expect(bypass, "GET", "#{username}/repos", fn conn ->
+        conn
+        |> Plug.Conn.put_resp_header("content-type", "application/json")
+        |> Plug.Conn.resp(404, body)
+      end)
+
+      response = Client.get_user_repos(url, username)
+
+      expected_response = {:error, %Error{result: "Github user not found!", status: :not_found}}
+
+      assert response == expected_response
+    end
+
+    test "when there is a connection error, returns an error", %{bypass: bypass} do
+      username = "12345"
+
+      url = endpoint_url(bypass.port)
+
+      Bypass.down(bypass)
+
+      response = Client.get_user_repos(url, username)
+
+      expected_response = {:error, %Error{result: :econnrefused, status: :bad_request}}
 
       assert response == expected_response
     end
