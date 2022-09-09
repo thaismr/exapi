566373efbc3d8e3c49ab9fd1e3e400dc18957fef
thaismr <thaismr@users.noreply.github.com>
2022-09-09 18:52:02 -0300

    feat: add API auth test

--- lib/exapi_web/auth/error_handler.ex
+++ lib/exapi_web/auth/error_handler.ex
@@ -1,11 +1,16 @@
 defmodule ExapiWeb.Auth.ErrorHandler do
+  import Plug.Conn
+
   alias Guardian.Plug.ErrorHandler
-  alias Plug.Conn
 
   @behaviour ErrorHandler
 
+  @impl ErrorHandler
   def auth_error(conn, {error, _reason}, _opts) do
     body = Jason.encode!(%{message: to_string(error)})
-    Conn.send_resp(conn, 401, body)
+
+    conn
+    |> put_resp_content_type("application/json")
+    |> send_resp(401, body)
   end
 end
